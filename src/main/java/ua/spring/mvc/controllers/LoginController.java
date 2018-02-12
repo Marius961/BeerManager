b9fac34aac0c33e27a6002db638d8e0b8e5b819c
Marius <marius.shiman@gmail.com>
2018-02-12 15:37:47 +0200

    Code reformating

--- src/main/java/ua/spring/mvc/controllers/LoginController.java
+++ src/main/java/ua/spring/mvc/controllers/LoginController.java
@@ -2,12 +2,11 @@ package ua.spring.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import ua.spring.mvc.models.User;
+import ua.spring.mvc.objects.User;
 
 @Controller
 public class LoginController {
@@ -20,5 +19,10 @@ public class LoginController {
     @RequestMapping(value = "/check-user", method = RequestMethod.POST)
     public ModelAndView checkUser(@ModelAttribute("user") User user) {
         return new ModelAndView("main", "user", user);
+    }
+
+    @RequestMapping(value = "/failed", method = RequestMethod.GET)
+    public ModelAndView failed() {
+        return new ModelAndView("login-failed", "message", "Login failed");
     }
 }
---
+++ src/main/java/ua/spring/mvc/interceptors/CheckUserInterceptor.java
@@ -0,0 +1,24 @@
+package ua.spring.mvc.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import ua.spring.mvc.objects.User;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CheckUserInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        if (request.getRequestURI().contains("check-user")) {
+
+            User user = (User) modelAndView.getModel().get("user");
+            if (user.getName().equals("admin")) {
+                response.sendRedirect(request.getContextPath() + "/failed");
+            }
+
+        }
+    }
+}
