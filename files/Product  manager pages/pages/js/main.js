2e52de472feff915947eee43d3b87c1775cfa27f
Marius <marius.shiman@gmail.com>
2018-10-15 21:49:31 +0300

    added product card prototype

--- files/Product  manager pages/pages/js/main.js
+++ files/Product  manager pages/pages/js/main.js
@@ -1,9 +1,42 @@
+let togleAnimationTime = 300,
+    categoriesContainer = $(".categories-container"),
+    scrollAnimationClock = 80,
+    firefoxscrollAnimationClock = 40;
+
+
+
+
 function showCategories() {
     let elem = $(".categories-wrapper");
     if ($(elem).css("display") === "none") {
-        $(elem).slideDown(300);
+        $(elem).slideDown(togleAnimationTime);
     } else {
-        $(elem).slideToggle(300);
+        $(categoriesContainer).stop().animate({scrollLeft: 0}, 300);
+        let togglingTimeOut = 0;
+        if ($(categoriesContainer)[0].scrollLeft !== 0) {
+            togglingTimeOut = togleAnimationTime;
+        }
+        setTimeout(function () {
+            $(elem).slideToggle(togleAnimationTime);
+        }, togglingTimeOut);
     }
 }
 
+$(document).ready(function() {
+    $(categoriesContainer).bind('mousewheel', function(e) {
+        e.preventDefault();
+        let containerWidth = parseFloat($(categoriesContainer).css("width"));
+        // noinspection JSCheckFunctionSignatures
+        $(categoriesContainer).stop().animate({scrollLeft: String('-=' + (-containerWidth / e.originalEvent.wheelDelta)*scrollAnimationClock)}, 300);
+    });
+    $(categoriesContainer).bind('mousewheel', function(e) {
+        e.preventDefault();
+        let containerWidth = parseFloat($(categoriesContainer).css("width"));
+        // noinspection JSCheckFunctionSignatures
+        $(categoriesContainer).stop().animate({scrollLeft: String('+=' + (-containerWidth / e.originalEvent.wheelDelta)*firefoxscrollAnimationClock)}, 300);
+    });
+    // $('.categories-container').bind('DOMMouseScroll', function(e) {//Speacially for Mozilla
+    //     this.scrollLeft += (e.originalEvent.detail*40);//Multiply by 40 because Mozilla DOMMouseScroll speed is 3px at time to match scrolling speed of chrome(speed of scrolling 120px at time)
+    // });
+
+});
