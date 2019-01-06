b41a2b23dc8a3ff0162b316336ecf43529c3281c
Marius <marius.shiman@gmail.com>
2019-01-06 20:14:37 +0200

    new design for the categories bar
    *CSS and JS code optimization

--- files/Product manager pages/pages/js/main.js
+++ files/Product manager pages/pages/js/main.js
@@ -1,4 +1,4 @@
-let animationTime = 200;
+let animationTime = 150;
 
 
 
@@ -28,7 +28,6 @@ $(".p-drop-btn").on("click", function () {
     let menu = $(this.parentNode).find(".p-dropdown");
     if ($(menu).css("display") === "none") {
         clearDropdowSelection();
-        clearCategorySelection();
         closeCategoriesMenu();
         $(menu).css("display", "inline-block")
     } else {
@@ -36,9 +35,6 @@ $(".p-drop-btn").on("click", function () {
     }
 });
 
-function clearDropdowSelection() {
-    $(".p-dropdown").css("display", "none");
-}
 
 $("#closeAddressFormBtn").on("click", function () {
     popupFormFadeToggle();
@@ -50,10 +46,10 @@ $("#addAddressBtn").click(function () {
 
 
 /*
-    * Resize address form window popu[]
+    * Resize address form window popup
 */
 $(".popup-bg").ready(function () {
-    $(".popup-bg").css('height', $(document).height())
+    $(".popup-bg").css('height', $(document).height());
     $(window).resize(function () {
         $(".popup-bg").css('height', $(document).height())
     });
@@ -63,8 +59,90 @@ $(".popup-bg").ready(function () {
 /*
     * Toggle address form visibility
 */
+
+$("#menu").on("click", function () {
+    $("#menuContent").slideToggle(150);
+});
+
+$("#categoriesBtn").click(function () {
+    clearDropdowSelection();
+    categoriesMenuSlideToggle();
+});
+
+$(".c-menu-item-box").mouseover(function (e) {
+    activateCategory(this);
+});
+
+$(".subcategories").mouseleave(function () {
+    deactivateCategory(this);
+});
+
+$(".categories-segment").mouseleave(function () {
+    deactivateCategory($(".subcategories"));
+});
+
+$("#filtersToggle").on("click", function () {
+    let filtersContainer = $("#filters");
+    if ($(filtersContainer).hasClass("d-none")) {
+        $(filtersContainer).removeClass("d-none");
+    } else {
+        $(filtersContainer).addClass("d-none");
+    }
+});
+
+function closeCategoriesMenu() {
+    let menuBtn = $("#categoriesBtn");
+    if($(menuBtn)[0] !== undefined) {
+        let elem = $(".categories-segment");
+        if ($(elem).css("display") === "flex") {
+            categoriesMenuSlideToggle();
+        }
+    }
+}
+
+function showSubcategories(container) {
+    $(container).removeClass("d-none");
+}
+
+function hideSubcategories(container) {
+    $(container).addClass("d-none");
+}
+
+function hideAllSubcategories() {
+    $(".subcategories").addClass("d-none");
+}
+
+function addCategoryActiveClass(category) {
+    $(category).addClass("c-menu-item-box-active");
+}
+
+function cleanActiveCategories() {
+    $(".c-menu-item-box-active").removeClass("c-menu-item-box-active");
+}
+
+
+function activateCategory(category) {
+    let subcategoriesContainer = $(category.parentNode).find(".subcategories");
+    cleanActiveCategories();
+    hideAllSubcategories();
+    addCategoryActiveClass(category);
+    showSubcategories(subcategoriesContainer);
+}
+
+function deactivateCategory(category) {
+    hideSubcategories(category);
+    cleanActiveCategories();
+}
+
+
 function popupFormFadeToggle() {
     $(".popup-bg").fadeToggle(150);
 }
 
+function clearDropdowSelection() {
+    $(".p-dropdown").css("display", "none");
+}
 
+function categoriesMenuSlideToggle() {
+    $(".categories-segment").animate({width:'toggle'},animationTime);
+}
