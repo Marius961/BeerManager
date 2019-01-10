a0c6d3cde5a3fa4b85da7304f8f2a50ab678fccb
Marius <marius.shiman@gmail.com>
2019-01-10 16:42:25 +0200

    add new items in the menu
    *tеhe user menu is moved to the general menu

--- files/Product manager pages/pages/js/main.js
+++ files/Product manager pages/pages/js/main.js
@@ -2,18 +2,21 @@ let animationTime = 150;
 
 
 
-
 /*
     * Method for calculating price
 */
 $("#measurementInput").on("input", function () {
+    let quantity = +$(this).val();
     let sumBox = $(".sum-box");
-    if ($(this).val() !== "") {
-        let sum = +$("#price").text() * +$(this).val();
-        $(sumBox).find("#sum").text(sum.toFixed(2) + "грн");
-        $(sumBox).fadeIn(510);
+        if (quantity) {
+        let productPrice = $("#price").text(),
+            sum = (productPrice * quantity).toFixed(2),
+            sumLabel = $(sumBox).find("#sum");
+
+        $(sumLabel).text(sum + "грн");
+        $(sumBox).slideDown(200);
     } else {
-        $(sumBox).fadeOut(300);
+        $(sumBox).slideUp(200);
     }
 });
 
@@ -28,17 +31,22 @@ $(".p-drop-btn").on("click", function () {
     let menu = $(this.parentNode).find(".p-dropdown");
     if ($(menu).css("display") === "none") {
         clearDropdowSelection();
-        closeCategoriesMenu();
         $(menu).css("display", "inline-block")
     } else {
         $(menu).css("display", "none")
     }
 });
 
+$(".p-dropdown").mouseleave(function () {
+    $(this).css("display", "none")
+});
+
 
 $("#closeAddressFormBtn").on("click", function () {
     popupFormFadeToggle();
 });
+
+
 
 $("#addAddressBtn").click(function () {
     popupFormFadeToggle();
@@ -56,30 +64,99 @@ $(".popup-bg").ready(function () {
 });
 
 
-/*
-    * Toggle address form visibility
-*/
 
-$("#menu").on("click", function () {
-    $("#menuContent").slideToggle(150);
-});
-
-$("#categoriesBtn").click(function () {
+$("#menuBtn").click(function () {
     clearDropdowSelection();
-    categoriesMenuSlideToggle();
+    slideUpAllItemContent();
+    menuSlideToggle();
 });
 
-$(".c-menu-item-box").mouseover(function (e) {
-    activateCategory(this);
+
+
+
+
+
+//
+// $(".category-item-box").mouseover(function () {
+//
+//     let subcategoriesContainer = $(this.parentNode).find(".subcategories");
+//     removeAllCategoryActiveClasses();
+//     hideAllSubcategories();
+//     addCategoryActiveClass(this);
+//     showSubcategories(subcategoriesContainer);
+//     $(subcategoriesContainer).mouseleave(function () {
+//         hideSubcategories(subcategoriesContainer);
+//         removeAllCategoryActiveClasses();
+//     })
+// });
+
+
+
+
+$(".category-item-box, .subcategories").hover(function () {
+    $(this.parentNode).find(".subcategories").toggleClass("d-none");
+    $(this).toggleClass("c-menu-item-box-active");
+    let k = $(this);
 });
 
-$(".subcategories").mouseleave(function () {
-    deactivateCategory(this);
-});
+$(".menu-item").click(function () {
+    let itemContent = $(this.parentNode).find(".item-content")
 
-$(".categories-segment").mouseleave(function () {
-    deactivateCategory($(".subcategories"));
-});
+    if ($(itemContent).css("display") === "none") {
+        slideUpAllItemContent();
+    }
+    $(itemContent).slideToggle(animationTime)
+})
+
+function slideUpAllItemContent() {
+    $(".item-content").slideUp(animationTime);
+}
+
+
+//
+// $(".categories-segment").mouseleave(function () {
+//     hideAllSubcategories();
+//     let menuTimerId = setTimeout(function () {
+//         menuSlideToggle()
+//     }, 500 );
+//     $(this).mouseenter(function () {
+//         clearTimeout(menuTimerId);
+//     })
+// });
+//
+//
+//
+// function showSubcategories(container) {
+//     $(container).removeClass("d-none");
+// }
+//
+// function hideSubcategories(container) {
+//     $(container).addClass("d-none");
+// }
+//
+// function hideAllSubcategories() {
+//     $(".subcategories").addClass("d-none");
+// }
+//
+// function addCategoryActiveClass(category) {
+//     $(category).addClass("c-menu-item-box-active");
+// }
+//
+// function removeAllCategoryActiveClasses() {
+//     $(".c-menu-item-box-active").removeClass("c-menu-item-box-active");
+// }
+//
+function menuSlideToggle() {
+    $("#menu").slideToggle(animationTime);
+}
+
+
+
+//
+// function categoriesMenuSlideUp() {
+//     $(".categories-segment").slideUp(animationTime);
+// }
+
 
 $("#filtersToggle").on("click", function () {
     let filtersContainer = $("#filters");
@@ -90,59 +167,10 @@ $("#filtersToggle").on("click", function () {
     }
 });
 
-function closeCategoriesMenu() {
-    let menuBtn = $("#categoriesBtn");
-    if($(menuBtn)[0] !== undefined) {
-        let elem = $(".categories-segment");
-        if ($(elem).css("display") === "flex") {
-            categoriesMenuSlideToggle();
-        }
-    }
-}
-
-function showSubcategories(container) {
-    $(container).removeClass("d-none");
-}
-
-function hideSubcategories(container) {
-    $(container).addClass("d-none");
-}
-
-function hideAllSubcategories() {
-    $(".subcategories").addClass("d-none");
-}
-
-function addCategoryActiveClass(category) {
-    $(category).addClass("c-menu-item-box-active");
-}
-
-function cleanActiveCategories() {
-    $(".c-menu-item-box-active").removeClass("c-menu-item-box-active");
-}
-
-
-function activateCategory(category) {
-    let subcategoriesContainer = $(category.parentNode).find(".subcategories");
-    cleanActiveCategories();
-    hideAllSubcategories();
-    addCategoryActiveClass(category);
-    showSubcategories(subcategoriesContainer);
-}
-
-function deactivateCategory(category) {
-    hideSubcategories(category);
-    cleanActiveCategories();
-}
-
-
-function popupFormFadeToggle() {
-    $(".popup-bg").fadeToggle(150);
-}
-
 function clearDropdowSelection() {
     $(".p-dropdown").css("display", "none");
 }
 
-function categoriesMenuSlideToggle() {
-    $(".categories-segment").animate({width:'toggle'},animationTime);
+function popupFormFadeToggle() {
+    $(".popup-bg").fadeToggle(animationTime);
 }
