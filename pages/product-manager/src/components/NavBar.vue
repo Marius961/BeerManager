b872d5689f5f3189c93ed38fe6a7f86f433c39c8
Marius <marius.shiman@gmail.com>
2019-02-08 18:43:00 +0200

    added cart items. calculating total price

--- pages/product-manager/src/components/NavBar.vue
+++ pages/product-manager/src/components/NavBar.vue
@@ -1,11 +1,11 @@
 <template>
   <header >
-    <div class="nav-container-bg container-fluid">
-      <nav class="container p-0">
-        <div class="row align-items-center">
-          <div class="col-6 col-sm-auto" id="menuBtn">
+    <div class="container-fluid nav-bar-container">
+      <div class="container">
+        <nav class="row nav-bar align-items-center">
+          <div class="col-6 col-sm-auto menu-button" @click="hideVisibleDropdowns()">
             <div class="row">
-              <div class="col-auto nav-menu-btn pt-1 pb-1">
+              <div class="col-auto  pt-1 pb-1">
                 <div class="row align-items-center no-gutters">
                   <div class="col-auto">
                     <div class="menu-dot"></div>
@@ -22,43 +22,24 @@
                 </div>
               </div>
             </div>
-          </div>
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 position-absolute" id="navMenu">
-            <div class="row containe">
-              <div class="col nav-menu">
-                <a href="#" class="row p-2">
-                  <div class="col-3 text-center">
-                    <img src="../assets/img/nav-bar/home.png" width="24" height="24">
-                  </div>
-                  <div class="col pt-1">На головну</div>
-                </a>
-                <div class="row">
-                  <div class="col">
-                    <div class="row p-2 nav-menu-item">
-                      <div class="col-3 text-center">
-                        <img src="../assets/img/nav-bar/categories.png" width="24" height="24">
-                      </div>
-                      <div class="col">Категорії</div>
+            <div class="nav-menu-wrapper">
+              <div class="row w-100">
+                <div class="col-12  nav-menu">
+                  <a href="#" class="row p-2">
+                    <div class="col-3 text-center">
+                      <img src="../assets/img/test/category.png" width="24" height="24">
                     </div>
-                    <div class="row">
-                      <div class="col menu-item-content-wrapper">
-                        <div class="row" id="categories">
-                          <div class="col-12 category-item">
-                            <div class="row p-2">
-                              <div class="col-3 text-center">
-                                <img src="../assets/img/test/category.png" width="32" height="32">
-                              </div>
-                              <div class="col">Пекарня</div>
-                            </div>
-                            <div class="row">
-                              <div class="col category-item-content-wrapper">
-                                <div class="row p-2">
-                                  <a class="col-6 subcategory p-1" href="#">Хліб</a>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                    <div class="col pt-1">На головну</div>
+                  </a>
+                  <div class="row t-text pl-3">Категорії</div>
+                  <div class="row">
+                    <div class="col-12">
+                      <div class="row item-body" @click="toggleDropdownSlide($event)">
+                        <div class="col-12">Категорія</div>
+                      </div>
+                      <div class="row item-body-content show-content">
+                        <div class="col-12">Категорія 1</div>
+                        <div class="col-12">Категорія 1</div>
                       </div>
                     </div>
                   </div>
@@ -69,54 +50,50 @@
           <div class="col-6 col-sm">
             <div class="row justify-content-end m-0"  style="flex-wrap: nowrap">
               <div class="col-auto nav-dropdown">
-                <div class="row nav-dropdown-btn align-items-center">
+                <div class="row align-items-center">
                   <div class="col-auto">
                     <img src="../assets/img/nav-bar/search.png" alt="">
                   </div>
                 </div>
               </div>
-              <div class="col-auto nav-dropdown">
-                <div class="row align-items-center">
+              <div class="col-auto">
+                <div class="row align-items-center nav-dropdown">
                   <div class="col-2">
                     <img src="../assets/img/nav-bar/user-icon.png" alt="">
                   </div>
                 </div>
-                <div class="nav-dropdown-menu user-actions-dropdown mt-2">
+                <div class="nav-dropdown-menu user-actions-dropdown">
                   <div class="row no-gutters">
-                    <a class="col-12 p-2 pl-2" href="#">Моє меню</a>
+                    <a class="col-12" href="#">Мої замовлення</a>
+                    <a class="col-12" href="#">Отримані замовлення</a>
+                    <a class="col-12" href="#">Мої товари</a>
                     <hr class="w-100 m-1">
-                    <a class="col-12 p-2 pl-2" href="#">Вийти</a>
+                    <a class="col-12" href="#">Вийти</a>
                   </div>
                 </div>
               </div>
-              <div class="col-auto nav-dropdown">
-                <div class="row align-items-center">
-                  <div class="col-2" style="position: absolute; z-index: 1; top: 11px; right: 14px">
-                    <div class="row align-items-center">
-                      <div class="col">
-                        <div class="nav-notifications-count-icon">999</div>
-                      </div>
-                    </div>
-                  </div>
+              <div class="col-auto">
+                <div class="row align-items-center nav-dropdown">
+                  <div class="cart-items-count">933</div>
                   <div class="col-2">
                     <img src="../assets/img/nav-bar/cart-icon.png" alt="">
                   </div>
                 </div>
-                <div class="nav-dropdown-menu cart-dropdown-menu mt-2">
+                <div class="nav-dropdown-menu cart-dropdown-menu">
                   <div class="row m-0">
                     <div class="col-12">
                       <div class="row nav-cart-box p-1">
                         <div class="col">
-                          <div class="row nav-cart-item align-items-center p-3 mt-1 mb-1">
+                          <div v-for="item in cartItems" class="row nav-cart-item align-items-center p-3 mt-1 mb-1">
                             <div class="col-4 col-sm-2 text-center">
-                              <img src="../assets/img/test/product-photo.png" alt="">
+                              <img :src="item.imageSrc" alt="">
                             </div>
-                            <div class="col-8 col-sm-10 align-self-center text-center nav-cart-item-text">Апельсин</div>
+                            <div class="col-8 col-sm-10 align-self-center text-center nav-cart-item-text">{{item.name}}</div>
                             <hr class="w-100">
                             <div class="col">
                               <div class="row cart-item-info">
-                                <div class="col-6 text-center">32кг</div>
-                                <div class="col-6 text-center">19548 грн</div>
+                                <div class="col-6 text-center">{{item.quantity}}{{item.measurementUnit.shortName}}</div>
+                                <div class="col-6 text-center">{{item.priceForOneMeasurementUnit * item.quantity}} грн</div>
                               </div>
                             </div>
                           </div>
@@ -124,31 +101,72 @@
                       </div>
                     </div>
                     <hr>
-                    <div class="col-12 nav-cart-total-price p-2 mt-2 text-center">Сума: 99999грн</div>
+                    <div class="col-12 nav-cart-total-price p-2 mt-2 text-center">Сума: {{calculateCartSum()}}грн</div>
                     <button class="col-12 nav-go-to-cart-btn p-2 mt-2 text-center">Перейти у кошик</button>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     </div>
   </header>
 </template>
 
 <script>
+  let carIte1 = {
+    id: 0,
+    imageSrc: 'https://wp.miray.com.ua/site/miray/files/wallpapers/wallpaper-4187.jpg',
+    name: 'Апельсин',
+    priceForOneMeasurementUnit: 15.60,
+    quantity: 3,
+    measurementUnit: { id:0, shortName: 'кг', fullName: 'кілограм'},
+  };
+  let carIte2 = {
+    id: 1,
+    imageSrc: 'https://irecommend.ru/sites/default/files/product-images/42233/zwr0obbJCnszZYssy91zA.jpg',
+    name: 'Мука',
+    priceForOneMeasurementUnit: 20,
+    quantity: 2,
+    measurementUnit: { id:0, shortName: 'кг', fullName: 'кілограм'},
+  };
+  let carIte3 = {
+    id: 2,
+    imageSrc: 'https://i.biz-gid.com/img/products/800/216796.png',
+    name: 'Хліб',
+    priceForOneMeasurementUnit: 8.50,
+    quantity: 2,
+    measurementUnit: { id:0, shortName: 'шт', fullName: 'штук'},
+  };
+  let carIte4 = {
+    id: 2,
+    imageSrc: 'https://img2.zakaz.ua/src.1470729330.ad72436478c_2016-08-09_Aleksey/src.1470729330.SNCPSG10.obj.0.1.jpg.oe.jpg.pf.jpg.1350nowm.jpg.1350x.jpg',
+    name: 'Моршинська (сильногазована)',
+    priceForOneMeasurementUnit: 12.50,
+    quantity: 1,
+    measurementUnit: { id:0, shortName: 'л.', fullName: 'літрів'},
+  };
+
   export default {
     data() {
       return {
-
+        cartItems: [carIte1, carIte2, carIte3, carIte4],
+        cartSum: 0
       }
     },
     methods: {
+      calculateCartSum() {
+        let sum = 0;
+        this.cartItems.map(item => {
+          sum += item.priceForOneMeasurementUnit * item.quantity
+        });
+        return sum;
+      }
     }
   }
 </script>
 
 <style scoped>
-
+  @import "../assets/css/nav-bar.css";
 </style>
