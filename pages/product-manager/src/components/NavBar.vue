6a8e3bfb21edfbfeded86b2b16ccf0b66c994c0a
Marius <marius.shiman@gmail.com>
2019-02-10 20:00:17 +0200

    Add sidebar instead of menu

--- pages/product-manager/src/components/NavBar.vue
+++ pages/product-manager/src/components/NavBar.vue
@@ -1,13 +1,12 @@
 <template>
-  <header >
+  <header>
     <div class="container-fluid nav-bar-container">
       <div class="container">
         <nav class="row align-items-center">
           <div class="col-5 col-sm-auto menu-btn-container"
-               @click="showMenu"
-               @mouseenter="showMenu"
-               @mouseleave="hideMenu(menuHideDelay)">
-            <div class="row h-100 align-items-center menu-button" style="z-index: 99" @click="hideMenu(0)">
+               @click="isShowSideBar = !isShowSideBar"
+          >
+            <div class="row h-100 align-items-center menu-button" style="z-index: 99">
               <div class="col-auto">
                 <div class="row align-items-center no-gutters">
                   <div class="col-auto">
@@ -21,71 +20,9 @@
                 </div>
               </div>
             </div>
-            <div class="row pr-3 position-absolute" style="pointer-events: none">
-              <div class="col menu-container" :class="{'show-menu': isShowMenu}">
-                <div class="row">
-                  <div class="col-12 nav-menu">
-                    <div class="row menu-links-container align-items-center pt-2 pb-2">
-                      <a href="#" class="col-3 col-sm-2">
-                        <div class="row justify-content-center">
-                          <div class="col-auto text-center">
-                            <img src="../assets/img/nav-bar/home.png" width="24" height="24">
-                          </div>
-                        </div>
-                      </a>
-                      <span class="col font-weight-bold fs-4" style="border-left: 1px solid white">Меню</span>
-                    </div>
-                    <div class="row">
-                      <div class="col">
-                        <div class="row menu-item" :class="{'menu-item-active': showCategories}"  @click="showCategories = !showCategories">
-                          <div class="col">
-                            <div class="row align-items-center">
-                              <div class="col-9 text-left">Категорії</div>
-                              <div class="col-3">
-                                <img class="dropdonw-arrow" :class="{'dropdonw-arrow-opened': showCategories}" src="../assets/img/nav-bar/dropdown_arrow.png" alt="">
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row dropdown-content" :class="{'show-dropdown-content': showCategories}">
-                          <div class="col-12" v-for="category in categories" @click="category.isOpened = !category.isOpened">
-                            <div class="row category align-items-center" :class="{'category-active': category.isOpened}">
-                              <div class="col-auto p-1">
-                                <img src="../assets/img/test/category.png">
-                              </div>
-                              <div class="col">{{category.name}}</div>
-                            </div>
-                            <div class="row subcategories" v-if="category.isOpened">
-                              <a class="col-12 col-sm-6" v-for="subcategory in category.subcategories" :href="subcategory.id">{{subcategory.name}}</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <a class="col-12">
-                        <div class="row menu-item">
-                          <a href="#" class="col">Про нас</a>
-                        </div>
-                      </a>
-                      <a class="col-12">
-                        <div class="row menu-item">
-                          <a href="#" class="col">Оплата і доставка</a>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           <div class="col-7 col-sm">
             <div class="row justify-content-end m-0"  style="flex-wrap: nowrap">
-              <div class="col-auto nav-dropdown">
-                <div class="row h-100 align-items-center">
-                  <div class="col-auto">
-                    <img src="../assets/img/nav-bar/search.png" alt="">
-                  </div>
-                </div>
-              </div>
               <div class="col-auto">
                 <div class="row align-items-center nav-dropdown">
                   <div class="col-2">
@@ -139,6 +76,63 @@
             </div>
           </div>
         </nav>
+      </div>
+    </div>
+    <div class="sidebar-wrapper"
+         @click.self="closeSideBar"
+         :class="{'show-sidebar-wrapper': isShowSideBar}"
+    >
+      <div class="sidebar" :class="{'show-sidebar': isShowSideBar}">
+        <div class="row no-gutters">
+          <div class="col-12">
+            <div class="row align-items-center menu-title-bar">
+              <a href="#" class="col-2 pb-2">
+                <img src="../assets/img/nav-bar/home.png" alt="">
+              </a>
+              <div class="col">
+                <h4>Меню</h4>
+              </div>
+              <div class="col-2" @click="closeSideBar">
+                <img src="../assets/img/nav-bar/close.png" alt="">
+              </div>
+            </div>
+          </div>
+          <hr class="w-100 m-1">
+          <div class="col-12">
+            <div class="row no-gutters">
+              <div class="col-12 sidebar-item" @click="isShowCategories = !isShowCategories" :class="{'sidebar-item-active': isShowCategories}">Категорії</div>
+              <div class="col-12 item-dropdown" v-if="isShowCategories">
+                <div v-for="category in categories" class="row align-items-center">
+                  <div class="col">
+                    <div class="row category" @click="category.isOpened = !category.isOpened">
+                      <div class="col-2">
+                        <img src="../assets/img/test/category.png" alt="">
+                      </div>
+                      <div class="col">{{category.name}}}</div>
+                    </div>
+                    <div class="row">
+                      <div class="col-12 p-2" v-if="category.isOpened">
+                        <div class="row no-gutters">
+                          <a v-for="subcategory in category.subcategories" href="#" class="col-12 subcategory">{{subcategory.name}} 1</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row no-gutters sidebar-item">Про нас</div>
+            <div class="row no-gutters sidebar-item">Оплата та доставка</div>
+            <div class="row no-gutters sidebar-item">Правила розміщення товарів</div>
+            <div class="row no-gutters p-2 search-field">
+              <label for="search" class="col-12">Пошук по сайту</label>
+              <input id="search" class="col-9" type="search">
+              <div class="col-3 text-center">
+                <img src="../assets/img/nav-bar/search.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </header>
@@ -215,11 +209,9 @@
       return {
         cartItems: [carIte1, carIte2, carIte3, carIte4],
         cartSum: 0,
-        showCategories: false,
         categories: [category, category2],
-        isShowMenu: false,
-        menuCloseTimerId: '',
-        menuHideDelay: 300
+        isShowSideBar: false,
+        isShowCategories: false,
       }
     },
     methods: {
@@ -230,20 +222,13 @@
         });
         return sum;
       },
-      showMenu() {
-        this.isShowMenu = true;
-        clearTimeout(this.menuCloseTimerId);
-      },
-      hideMenu(delay) {
-        this.menuCloseTimerId = setTimeout(() => {
-          this.isShowMenu = false;
-          this.showCategories = false;
-          this.hideAllSubcategories();
-        }, delay)
-      },
-      hideAllSubcategories() {
-        this.categories.map(category => {
-          category.isOpened = false;
+      closeSideBar() {
+        this.isShowSideBar = false;
+        setTimeout(() => {
+          this.isShowCategories = false;
+          this.categories.map(category => {
+            category.isOpened = false;
+          }, 300)
         })
       }
     }
@@ -252,6 +237,7 @@
 
 <style scoped>
   @import "../assets/css/nav-bar.css";
+
 
 
 </style>
