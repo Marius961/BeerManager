7869884edf19ee5aff7f977765dc731d941c619f
Marius <marius.shiman@gmail.com>
2019-02-11 18:39:36 +0200

    new product card design

---
+++ pages/product-manager/src/components/ProductCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="p-4">
+    <div class="row justify-content-center product-card">
+      <div class="col-12 card-img">
+        <div class="row h-100 align-items-center">
+          <div class="col-12 text-center">
+            <img :src="imgSrc" alt="">
+          </div>
+          <div v-if="inStockStatus" class="col-auto in-stock-label">В наявності</div>
+        </div>
+      </div>
+      <div class="col-12 card-body align-self-start">
+        <div class="row">
+          <div class="col-12 product-name">{{productName}}</div>
+          <div class="col-12 seller-nick">Продавець: {{sellerNickname}}</div>
+          <div class="col-12 pt-3">
+            <div class="row align-items-end justify-content-between">
+              <div class="col-auto">Ціна:</div>
+              <div class="col-auto text-right product-price">{{priceForMeasurementUnit}} грн/{{measurementUnit}}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 align-self-end">
+        <div class="row align-items-center justify-content-between pt-1 pb-2">
+          <hr class="w-100 m-0 mt-1 mb-1">
+          <a href="#" class="col-auto details-btm">Деталі</a>
+          <div class="col-auto text-center">
+            <img src="../assets/img/heart.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: {
+        id: Number,
+        imgSrc: String,
+        productName: String,
+        priceForMeasurementUnit: Number,
+        measurementUnit: String,
+        sellerNickname: String,
+        inStockStatus: Boolean,
+        isInFavorites: Boolean
+      }
+    }
+</script>
+
+<style scoped>
+  @import "../assets/css/product-card.css";
+</style>
--- pages/product-manager/src/pages/Home.vue
+++ pages/product-manager/src/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <main class="container">
     <div class="row">
-      <div class="col-12 mt-sm-3" id="categoriesList">
-        <div class="row" style="overflow-x: auto; flex-wrap: nowrap">
-          <a href="#" class="col-7 col-sm-5 col-md-3 col-lg-2 category-card p-3">
+      <div class="col-12 mt-sm-3">
+        <div class="row cards-bar">
+          <a v-for="c in 6" href="#" class="col-7 col-sm-5 col-md-3 col-lg-2 category-card p-3">
             <div class="row align-items-start text-center">
               <div class="col">
                 <img src="../assets/img/test/category.png" alt="">
@@ -13,49 +13,50 @@
           </a>
         </div>
       </div>
-      <div class="col-12 box mt-3 p-2 pl-3 pr-3">
+      <div  class="col-12 mt-3 p-2 pl-3 pr-3">
         <div class="row">
           <div class="col-12 col-sm-auto font-weight-bold">Популярні товари в категорії &laquo;пекарня&raquo;</div>
           <a href="#" class="col link-1 text-right font-weight-light">Перейти до категорії</a>
           <hr class="w-100 m-1">
           <div class="col-12">
-            <div class="row no-gutters row-eq-height" id="products-container" style="overflow-x: auto; overflow-y: hidden; flex-wrap: nowrap">
-              <div class="col-12 col-sm-6 col-md-4 col-xl-3 product-card p-2 p-xl-0">
-                <div class="row pb-3 no-gutters">
-                  <div class="col-12 p-3">
-                    <div class="col-12 text-center pt-2 product-name">Апельсин</div>
-                  </div>
-                  <div class="col-12 align-self-end">
-                    <div class="row justify-content-center no-gutters">
-                      <div class="col-auto">
-                        <img class="product-picture" src="https://st.depositphotos.com/1000141/1936/i/950/depositphotos_19366981-stock-photo-one-ripe-orange-in-closeup.jpg">
-                      </div>
-                      <div class="w-100"></div>
-                      <div class="col-8 col-sm-10 col-lg-9 col-xl-10 mt-4">
-                        <div class="row no-gutters justify-content-center">
-                          <div class="col-12 col-sm-9 price-box">
-                            <div class="row no-gutters p-2">
-                              <span class="col-auto text-right align-self-center">99999.99</span>
-                              <span class="col-auto price-currency text-left pl-1">грн/кг</span>
-                            </div>
-                          </div>
-                          <div class="col-12 col-sm-3 to-cart-segment">
-                            <div class="row no-gutters p-1 p-lg-2 h-100 align-items-center">
-                              <div class="col text-center">
-                                <img src="../assets/img/to_cart_icon_white.png" height="24" width="24">
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <div class="row">
+              <product-card
+                class="col-12 col-sm-6 col-md-4 col-xl-3"
+                productName="Молоко"
+                imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdr8i-0nIWMk3EW39bbDrQMvX3ucvU8R9_Bb82i6Ui54qO2EUC"
+                priceForMeasurementUnit="21,90"
+                measurementUnit="л."
+                sellerNickname="marius961"
+                inStockStatus="true"
+                isInFavorites="false"
+              >
+              </product-card>
+              <product-card
+                class="col-12 col-sm-6 col-md-4 col-xl-3"
+                productName="Молоко"
+                imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqowTiH5C4Ywszo5AXBxX9NrcEedr3j1CY7-n2hf7SWkrAMv47DQ"
+                priceForMeasurementUnit="21,90"
+                measurementUnit="л."
+                sellerNickname="marius961"
+                inStockStatus="true"
+                isInFavorites="false"
+              >
+              </product-card>
+              <product-card
+                class="col-12 col-sm-6 col-md-4 col-xl-3"
+                productName="Молоко"
+                imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKtngGfEjZwPMFDFGOmxTeyrrObo-GE0Y44GSxKtdtINAZaL_P"
+                priceForMeasurementUnit="21,90"
+                measurementUnit="л."
+                sellerNickname="marius961"
+                inStockStatus="true"
+                isInFavorites="false"
+              >
+              </product-card>
             </div>
           </div>
-          <hr class="w-100 m-1">
           <a href="#" class="col link-1 text-right font-weight-light">показати більше</a>
+          <hr class="w-100 m-1">
         </div>
       </div>
     </div>
@@ -63,8 +64,11 @@
 </template>
 
 <script>
+  import ProductCard from '../components/ProductCard'
     export default {
-        name: "Home"
+        components: {
+          "product-card": ProductCard
+        }
     }
 </script>
 
