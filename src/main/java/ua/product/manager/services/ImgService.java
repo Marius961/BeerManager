73b34241f49ff2071605ddde3f5a2c90abf4c999
Marius <marius.shiman@gmail.com>
2019-05-31 14:15:16 +0300

    File compression added

--- src/main/java/ua/product/manager/services/ImgService.java
+++ src/main/java/ua/product/manager/services/ImgService.java
@@ -5,10 +5,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -37,7 +44,30 @@ public class ImgService {
                 if (isExstensionValid && isUploadFolderExists) {
                     String uuidFile = UUID.randomUUID().toString();
                     String resultFileName = uuidFile + "." + file.getOriginalFilename();
-                    file.transferTo(new File(uploadPath + '/' + resultFileName));
+
+                    File convertedFile = convertMultiPartToFile(file);
+
+                    BufferedImage image = ImageIO.read(convertedFile);
+                    Files.deleteIfExists(convertedFile.toPath());
+
+
+                    File compressedImageFile = new File(uploadFolder.getPath().concat('/' + resultFileName));
+                    OutputStream os = new FileOutputStream(compressedImageFile);
+
+                    Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpg");
+                    ImageWriter writer = writers.next();
+
+                    ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+                    writer.setOutput(ios);
+
+                    ImageWriteParam param = writer.getDefaultWriteParam();
+
+                    param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+                    param.setCompressionQuality(0.07f);
+                    writer.write(null, new IIOImage(image, null, null), param);
+                    os.close();
+                    ios.close();
+                    writer.dispose();
                     return resultFileName;
                 } else throw new IOException("File extension is not valid. Valid file extensions: " + Arrays.toString(ALLOWED_EXTENSIONS));
             } else throw new InvalidFileNameException(null, "Invalid file name");
@@ -54,5 +84,13 @@ public class ImgService {
             if (e.equalsIgnoreCase(extension)) return true;
         }
         return false;
+    }
+
+    private File convertMultiPartToFile(MultipartFile file ) throws IOException {
+        File convFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
+        FileOutputStream fos = new FileOutputStream( convFile );
+        fos.write( file.getBytes() );
+        fos.close();
+        return convFile;
     }
 }
